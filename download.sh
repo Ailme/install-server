ad002f27b134e6214078f58521cb5b7d56ac99f3
Alexandr Tumaykin <alexandrtumaykin@gmail.com>
2015-12-03 23:46:04 +0300

    modified:   download.sh

--- download.sh
+++ download.sh
@@ -5,9 +5,14 @@ set -o pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-if [[ "$UID" -ne "0" ]]; then
+[[ "$UID" -ne "0" ]]; then
 	echo 'You must be root'
 	exit 1
+fi
+
+if [[ ! $(which unzip) ]]; then
+    echo "Need install unzip"
+    apt-get install -y unzip
 fi
 
 COLORIZER_URI="https://github.com/jakobwesthoff/colorizer/archive/master.zip"
@@ -17,8 +22,5 @@ REPOSITORY_URI="https://github.com/Ailme/install-server/archive/master.zip"
 
 cd /tmp
 
-mkdir install-server && cd install-server
-wget $REPOSITORY_URI && unzip master.zip && rm master.zip
-
-mkdir colorizer
-wget $COLORIZER_URI && unzip master.zip -d colorizer && rm master.zip
+wget --no-check-certificate $REPOSITORY_URI && unzip master.zip -d /tmp && rm master.zip
+wget --no-check-certificate $COLORIZER_URI | unzip master.zip -d /tmp  && rm master.zip
