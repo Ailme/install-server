ae8d15e0fecf410b16f2336ef412af737dc192c8
Alexandr Tumaykin <alexandrtumaykin@gmail.com>
2015-12-12 00:02:19 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -13,17 +13,20 @@ if [[ ! $(which unzip) ]]; then
     apt-get install -y unzip
 fi
 
+COLORIZER_ZIP=colorizer.zip
+REPO_ZIP=colorizer.zip
 COLORIZER_URI="https://github.com/jakobwesthoff/colorizer/archive/master.zip"
-REPOSITORY_URI="https://github.com/Ailme/install-server/archive/master.zip"
+REPO_URI="https://github.com/Ailme/install-server/archive/master.zip"
 
 ###
 
 cd /tmp
 
-wget --no-check-certificate $REPOSITORY_URI && unzip master.zip -d /tmp && rm master.zip
-wget --no-check-certificate $COLORIZER_URI && unzip master.zip -d /tmp  && rm master.zip
-
+wget --no-check-certificate -O $COLORIZER_ZIP $COLORIZER_URI && unzip $COLORIZER_ZIP -d /tmp
 [[ ! -d /usr/bin/colorizer-master ]] && mv /tmp/colorizer-master /usr/bin/
 [[ ! -r /usr/bin/colorizer ]] && ln -s /usr/bin/colorizer-master/Library/colorizer.sh /usr/bin/colorizer
+rm $COLORIZER_ZIP
 
+wget --no-check-certificate -O $REPO_ZIP $REPO_URI && unzip $REPO_ZIP -d /tmp
+rm $REPO_ZIP
 bash /tmp/install-server-master/install.sh
