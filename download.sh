8cd5ed2e18194b9c25085cb53a15200a64657a7a
Alexandr Tumaykin <alexandrtumaykin@gmail.com>
2015-12-12 00:08:31 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -22,11 +22,32 @@ REPO_URI="https://github.com/Ailme/install-server/archive/master.zip"
 
 cd /tmp
 
-wget --no-check-certificate -O $COLORIZER_ZIP $COLORIZER_URI && unzip $COLORIZER_ZIP -d /tmp
-[[ ! -d /usr/bin/colorizer-master ]] && mv /tmp/colorizer-master /usr/bin/
+echo "[$(date +%T)] download colorizer"
+wget --no-check-certificate -O $COLORIZER_ZIP $COLORIZER_URI
+
+echo "[$(date +%T)] unzip archive"
+unzip $COLORIZER_ZIP -d /tmp
+
+echo "[$(date +%T)] check and remove older version"
+[[ ! -d /usr/bin/colorizer-master ]] && rm -rf /usr/bin/colorizer-master
+
+echo "[$(date +%T)] copy"
+mv /tmp/colorizer-master /usr/bin/
+
+echo "[$(date +%T)] create link"
 [[ ! -r /usr/bin/colorizer ]] && ln -s /usr/bin/colorizer-master/Library/colorizer.sh /usr/bin/colorizer
+
+echo "[$(date +%T)] remove archive"
 rm $COLORIZER_ZIP
 
-wget --no-check-certificate -O $REPO_ZIP $REPO_URI && unzip $REPO_ZIP -d /tmp
+echo "[$(date +%T)] download installer"
+wget --no-check-certificate -O $REPO_ZIP $REPO_URI
+
+echo "[$(date +%T)] unzip archive"
+unzip $REPO_ZIP -d /tmp
+
+echo "[$(date +%T)] remove archive"
 rm $REPO_ZIP
+
+echo "[$(date +%T)] run install script"
 bash /tmp/install-server-master/install.sh
