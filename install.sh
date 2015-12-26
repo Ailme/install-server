964dd3ae4dd3874fb4f2c6846a15c125c3afd658
Alexandr Tumaykin <alexandrtumaykin@gmail.com>
2015-12-26 10:08:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,22 @@
 #set -eu
 #set -o pipefail
 
+export DEBIAN_FRONTEND=noninteractive
+
+CODENAME="$(cat /etc/os-release |grep VERSION= |cut -f 2 -d \(|cut -f 1 -d \))"
+
 if [[ "$UID" -ne "0" ]]; then
-	echo 'You must be root'
-	exit 1
+    echo 'You must be root'
+    exit 1
 fi
 
 ###
 
 cd /tmp
 
-DEFAULT_PACKAGES="lsb-release aptitude vim supervisor tmux htop mc wget sudo iftop tcpdump ntp man less strace \
- tcpdump telnet sysstat rsync lsof time screen iptraf-ng lftp ftp xterm jq lynx pandoc xmlstarlet curl dstat ncdu \
- nethogs pxz pigz pbzip2 autossh git cron inetutils-ping bash-completion xz-utils lxc debootstrap libvirt-bin \
+DEFAULT_PACKAGES="lsb-release aptitude vim supervisor tmux htop mc wget sudo iftop tcpdump ntp man less strace 
+ tcpdump telnet sysstat rsync lsof time screen iptraf-ng lftp ftp xterm jq lynx pandoc xmlstarlet curl dstat ncdu
+ nethogs pxz pigz pbzip2 autossh git cron inetutils-ping bash-completion xz-utils lxc debootstrap libvirt-bin
  bridge-utils ebtables dnsmasq dkms build-essential"
 
 [[ ! -d /root/.ssh ]] && mkdir /root/.ssh
@@ -24,24 +28,22 @@ DEFAULT_PACKAGES="lsb-release aptitude vim supervisor tmux htop mc wget sudo ift
 echo "Update timezone"
 read -e -p "Enter Timezone: " -i "Europe/Moscow" TIMEZONE
 
-echo $TIMEZONE > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata
+echo $TIMEZONE > /etc/timezone && dpkg-reconfigure -f tzdata
 
-read -e -p "Add jessie-backports repository? " -i "Y" YES
+read -e -p "Add $CODENAME backports repository? " -i "Y" YES
 if [[ "$YES" == "Y" || "$YES" == "y" ]]; then
-	echo "deb http://http.debian.net/debian jessie-backports main" >  /etc/apt/sources.list.d/backports.list
+	echo "deb http://http.debian.net/debian $CODENAME-backports main" >  /etc/apt/sources.list.d/backports.list
 	apt-get update
 
 	read -e -p "Upgrade kernel? " -i "n" YES
 	if [[ "$YES" == "Y" || "$YES" == "y" ]]; then
-		apt-cache search -t jessie-backports linux-image
-		echo -n "Paste linux-image name: "
-		read IMAGE
+		apt-cache search -t "$CODENAME"-backports linux-image
+		read -e -p "Select linux-image name: " IMAGE
 
-		apt-cache search -t jessie-backports linux-headers
-		echo -n "Paste linux-headers name: "
-		read HEADERS
+		apt-cache search -t "$CODENAME"-backports linux-headers
+		echo -e -p "Select linux-headers name: " HEADERS
 
-		apt-get install -t jessie-backports "$IMAGE" "$HEADERS"
+		apt-get install -t "$CODENAME"-backports "$IMAGE $HEADERS"
 
 		update-grub2
 	fi
